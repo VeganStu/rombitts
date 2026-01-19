a82a6cb5984517292d59d30fd73155777bdfc413
Stuart Gardiner <stugardiner1234@gmail.com>
2026-01-19 15:49:27 +0000

    Stage 2: split settings into env-ready package

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rombitts.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rombitts.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- rombitts/wsgi.py
+++ rombitts/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rombitts.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rombitts.settings.local')
+
 
 application = get_wsgi_application()
